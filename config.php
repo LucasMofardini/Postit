1a1ea8523d30511baf2ae661df4734e0bcc428e9
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2021-04-24 02:12:14 -0300

    Inserindo novos dados na tabela artigo

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$mysql = new mysqli('localhost','root','awsmysql1','postit');
+$mysql = new mysqli('localhost','root','','postit');
 $mysql->set_charset('utf8');
 if($mysql == false){
     echo 'Banco não está funcionando';
